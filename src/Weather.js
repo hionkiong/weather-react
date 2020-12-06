4e16fcf86d3647d5a594246f1d57c05aa7df2918
hionkiong <stellachoi007@gmail.com>
2020-12-06 17:20:00 +0000

    linked Location buttons to API

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Axios from "axios";
-import Locations from "./Boxes/Locations";
 import WeatherInfo from "./Boxes/WeatherInfo";
 import WeatherInfoDetails from "./Boxes/WeatherInfoDetails";
 import Header from "./Boxes/Header";
@@ -44,11 +43,68 @@ export default function Weather() {
     setCity(event.target.value);
   }
 
+  function displayLondon(event) {
+    event.preventDefault();
+    setCity("London");
+    search(city);
+  }
+  function displayMadrid(event) {
+    event.preventDefault();
+    setCity("Madrid");
+    search(city);
+  }
+  function displaySeoul(event) {
+    event.preventDefault();
+    setCity("Seoul");
+    search(city);
+  }
+  function displayTokyo(event) {
+    event.preventDefault();
+    setCity("Tokyo");
+    search(city);
+  }
+  function displayParis(event) {
+    event.preventDefault();
+    setCity("Paris");
+    search(city);
+  }
+
   if (ready) {
     return (
       <div className="wrapper">
         <Header />
-        <Locations />
+        <div className="Locations">
+          <div className="box a">
+            <strong> LOCATIONS </strong>
+            <ul className="cities">
+              <li>
+                <a href="/" onClick={displayLondon}>
+                  London
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={displayMadrid}>
+                  Madrid
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={displaySeoul}>
+                  Seoul
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={displayTokyo}>
+                  Tokyo
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={displayParis}>
+                  Paris
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <ThemeBox />
         <div className="Search">
           <div className="box b">
